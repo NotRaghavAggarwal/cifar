1b87b100825ab833cf5fecec447c7a6d48b9fbdc
Raghav Aggarwal <s4655393@login0.compute.eait.uq.edu.au>
2025-03-27 11:26:16 +1000

    Added logs

--- classify_code/slurm.sh
+++ classify_code/slurm.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 #SBATCH --job-name=cifar_resnet_train          # Job name
-#SBATCH --output=logs/cifar_resnet_train_%j.log  # Standard output and error log
+##SBATCH --output=logs/cifar_resnet_train_%j.log  # Standard output and error log
 #SBATCH --nodes=1
 #SBATCH --ntasks=1                            # Number of tasks (1 task in this case)
-#SBATCH --cpus-per-task=16                    # Number of CPUs per task (adjust as needed)
-#SBATCH --mem=32G                           # Memory per node
+#SBATCH --cpus-per-task=1                    # Number of CPUs per task (adjust as needed)
+##SBATCH --mem=32G                           # Memory per node
 #SBATCH --gres=gpu:1                         # Request 1 GPU
-#SBATCH --partition=gpu                      # Specify the GPU partition, adjust if needed
+#SBATCH --partition=a100                      # Specify the GPU partition, adjust if needed
 
 #SBATCH --mail-type=ALL                       # Email notifications for job begin, end, and failure
 #SBATCH --mail-user=aggarwalraghav29@gmail.com    # Replace with your email address
 
-source /home/Student/s4655393/demo/bin/activate /home/Student/s4655393/cifar/classify_code
+source /home/Student/s4655393
+conda activate /home/Student/s4655393
 
-pyhon test_cifar_resnet.py 
+python test_cifar_resnet.py 
